c4b63470164d5d18ac31663ccc351277ed345144
Jared Hanson <jaredhanson@gmail.com>
2013-06-20 08:25:03 -0700

    Rename test suite.

--- test/history.test.js
+++ test/history.test.js
@@ -1,7 +1,7 @@
 define(['history'],
 function(history) {
 
-  describe("websocket", function() {
+  describe("history", function() {
     
     it('shoud export singleton instance', function() {
       expect(history).to.be.an('object');
